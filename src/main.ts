009a77a1df9a894faccde8257e1c359bd21ad399
Ihor-Chupryna <igchup1985@gmail.com>
2024-04-26 11:52:29 +0300

    linter new config

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/greeting", greetingRouter);
 
-
 const PORT = 3300;
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
